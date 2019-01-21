295b6893c73977dfc1844b5e7d7c70dd54ca13a2
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-01-21 20:51:27 +0100

    added progress bar

--- src/pages/PodcastPage.js
+++ src/pages/PodcastPage.js
@@ -4,13 +4,16 @@ import audioFile from '../res/audios/vaterland.ogg';
 import ReactPlayer from "react-player";
 import playIcon from '../res/icons/play.svg';
 import pauseIcon from '../res/icons/pause.svg';
-
+import { ProgressBar } from "react-bootstrap";
+import Duration from "../utils/Duration";
 
 export default class PodcastPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPlaying: false
+      isPlaying: false,
+      played: 0,
+      duration: 0
     };
 
     this.play = this.play.bind(this);
@@ -26,19 +29,41 @@ export default class PodcastPage extends React.Component {
            } }>
 
         <div className='playerContainer'>
-          <img
-            src={ this.state.isPlaying ? pauseIcon : playIcon }
-            onClick={ this.state.isPlaying ? this.pause : this.play }/>
           <ReactPlayer
+            ref={ this.ref }
+            className='player'
             url={ audioFile }
             onProgress={ this.onProgress }
+            onDuration={ this.onDuration }
             playing={ this.state.isPlaying }
             config={ { file: { forceAudio: true } }
             }/>
+          <img
+            className='button'
+            src={ this.state.isPlaying ? pauseIcon : playIcon }
+            onClick={ this.state.isPlaying ? this.pause : this.play }
+            alt='Buttons'/>
+
+          <div className='seekBar'>
+            <ProgressBar now={ this.state.played * 100 }/>
+            <input
+              type='range' min={ 0 } max={ 1 } step='any'
+              value={ this.state.played }
+              onMouseDown={ this.onSeekMouseDown }
+              onTouchStart={ this.onSeekMouseDown }
+              onChange={ this.onSeekChange }
+              onTouchMove={ this.onSeekChange }
+              onMouseUp={ this.onSeekMouseUp }
+              onTouchEnd={ this.onSeekMouseUp }
+            />
+          </div>
+          <Duration seconds={ this.state.played * this.state.duration }/>
         </div>
       </div>
     )
   }
+
+  ref = player => this.player = player;
 
   play() {
     this.setState({ isPlaying: true });
@@ -46,5 +71,28 @@ export default class PodcastPage extends React.Component {
 
   pause() {
     this.setState({ isPlaying: false });
+  }
+
+  onProgress = state => {
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  };
+
+  onSeekChange = e => {
+    this.setState({ played: parseFloat(e.target.value) })
+  };
+
+  onSeekMouseDown = () => {
+    this.setState({ seeking: true })
+  };
+
+  onSeekMouseUp = e => {
+    this.setState({ seeking: false });
+    this.player.seekTo(parseFloat(e.target.value))
+  }
+
+  onDuration = (duration) => {
+    this.setState({ duration })
   }
 }
