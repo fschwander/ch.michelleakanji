3c77a4ccf99b6cc573bef96c39739126e0f60428
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-01-29 23:24:56 +0100

    added new pages for new structure

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link, Redirect, Route, Router, Switch } from 'react-router-dom';
+import { Link, Route, Router, Switch } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { createBrowserHistory } from 'history';
-import AudioFeaturePage from "../pages/AudioFeaturePage";
 import ContactPage from "../pages/ContactPage";
+import WorksPage from "../pages/WorksPage";
+import HomePage from "../pages/HomePage";
 
 export default class Navigation extends React.Component {
   render() {
@@ -13,24 +14,17 @@ export default class Navigation extends React.Component {
     return (
       <Router history={ history }>
         <div>
-          <Switch>
-            <Route path={ `/feature` } component={ AudioFeaturePage }/>
-            <Route path={ `/contact` } component={ ContactPage }/>
-            <Route path={ `/` } render={ () => <Redirect to={ `/feature` }/> }/>
-          </Switch>
-
           <Navbar id="Navigation" collapseOnSelect fixedTop>
             <Navbar.Header>
               <Navbar.Brand>
-                <Link to={ `/feature` }>Michelle Akanji</Link>
+                <Link to={ `/` }>Michelle Akanji</Link>
               </Navbar.Brand>
               <Navbar.Toggle/>
             </Navbar.Header>
-
             <Navbar.Collapse>
               <Nav pullRight>
-                <LinkContainer to={ `/feature` }>
-                  <NavItem eventKey={ 0 }>Audio</NavItem>
+                <LinkContainer to={ `/works` }>
+                  <NavItem eventKey={ 0 }>Werke</NavItem>
                 </LinkContainer>
                 <LinkContainer to={ `/contact` }>
                   <NavItem eventKey={ 1 }>Kontakt</NavItem>
@@ -38,6 +32,12 @@ export default class Navigation extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Switch>
+            <Route exact path={ `/` } component={ HomePage }/>
+            <Route path={ `/works` } component={ WorksPage }/>
+            <Route path={ `/contact` } component={ ContactPage }/>
+          </Switch>
         </div>
       </Router>
     );
