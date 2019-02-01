631e4256a73a902b205eec7cd62af566b90efea7
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-02-01 11:20:36 +0100

    added linked in icon to ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import iconLinkedIn from '../res/icons/linked_in.svg'
+import { Image } from "react-bootstrap";
 
 export default class ContactPage extends React.Component {
 
@@ -6,8 +8,15 @@ export default class ContactPage extends React.Component {
     return (
       <div className='ContactPage'>
         <div className='center-container'>
-          <h2>Michelle Akanji</h2>
-          <a href='mailto:kontakt@michelleakanji.ch'>kontakt@michelleakanji.ch</a>
+          <a className='link'
+             href='mailto:kontakt@michelleakanji.ch'>
+            kontakt@michelleakanji.ch
+          </a>
+          <a className='icon'
+             href={ 'https://www.linkedin.com/in/michelle-akanji-86345269/' }
+             target='_blank' rel="noopener noreferrer">
+            <Image src={ iconLinkedIn }/>
+          </a>
         </div>
       </div>
     )
