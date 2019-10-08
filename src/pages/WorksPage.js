8e027ae7070df98782b12f9ddd09e5d445420d3c
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-10-08 17:11:39 +0200

    using carousel also on smaller screens

--- src/pages/WorksPage.js
+++ src/pages/WorksPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import WorksCarousel from "../components/WorksCarousel";
-import AllWorkTiles from "../components/AllWorkTiles";
-
 
 export default class WorksPage extends React.Component {
 
   render() {
     return (
       <div className='WorksPage'>
-        {window.innerWidth > 900 ? <WorksCarousel/> : <AllWorkTiles/>}
+        <WorksCarousel/>
       </div>
     );
   }
