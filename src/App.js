026f36988f80f2fb3be8b75069ed4d647e6e5b07
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-24 10:43:53 +0100

    added react-snapshot

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './styles/Shared.scss';
 import Navigation from "./components/Navigation";
 import Layout from "./components/Layout";
-import { createHashHistory } from "history";
-import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import WorksPage from "./pages/WorksPage";
 import ContactPage from "./pages/ContactPage";
 import Vaterland from "./works/Vaterland";
@@ -16,35 +15,33 @@ import CriticalWhiteness from "./works/CriticalWhiteness";
 import Solange from "./works/Solange";
 import Negritude from "./works/Negritude";
 
-export default function App({ children }) {
-
-  const history = createHashHistory();
+export default function App({children}) {
 
   return (
-    <HashRouter history={ history }>
+    <Router>
       <div className="App">
         <Navigation/>
-        <Layout children={ children }>
+        <Layout children={children}>
 
           <Switch>
-            <Route path='/works' component={ WorksPage }/>
-            <Route path='/contact' component={ ContactPage }/>
+            <Route path='/works' component={WorksPage}/>
+            <Route path='/contact' component={ContactPage}/>
 
-            <Route path='/vaterland' component={ Vaterland }/>
-            <Route path='/der-wert-der-wahrheit' component={ DerWertDerWahrheit }/>
-            <Route path='/heimat-schreiben' component={ HeimatSchreiben }/>
-            <Route path='/protest-selfie' component={ ProtestSelfie }/>
-            <Route path='/schlimmstenfalls-eine-utopie' component={ SchlimmstenfallsEineUtopie }/>
-            <Route path='/spuren-im-sand' component={ SpurenImSand }/>
-            <Route path='/critical-whiteness' component={ CriticalWhiteness }/>
-            <Route path='/solange' component={ Solange }/>
-            <Route path='/w-wie-negritude' component={ Negritude }/>
+            <Route path='/vaterland' component={Vaterland}/>
+            <Route path='/der-wert-der-wahrheit' component={DerWertDerWahrheit}/>
+            <Route path='/heimat-schreiben' component={HeimatSchreiben}/>
+            <Route path='/protest-selfie' component={ProtestSelfie}/>
+            <Route path='/schlimmstenfalls-eine-utopie' component={SchlimmstenfallsEineUtopie}/>
+            <Route path='/spuren-im-sand' component={SpurenImSand}/>
+            <Route path='/critical-whiteness' component={CriticalWhiteness}/>
+            <Route path='/solange' component={Solange}/>
+            <Route path='/w-wie-negritude' component={Negritude}/>
 
             <Redirect from='/' to='/works'/>
           </Switch>
 
         </Layout>
       </div>
-    </HashRouter>
+    </Router>
   );
 }
