06837014dcb305bd69115cc2d1b8060fbcea14f6
FabianSchwander <fabian.schwander@students.fhnw.ch>
2021-03-06 13:19:42 +0100

    better implementation of navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,19 +3,21 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 export const Navigation = () => {
   return (
-    <Navbar id="Navigation" expand='sm'>
-      <Navbar.Brand href={'/works'}>Michelle Akanji</Navbar.Brand>
-      <Navbar.Toggle className='navbar-toggle'>
-        <span className='icon-bar'/>
-        <span className='icon-bar'/>
-        <span className='icon-bar'/>
-      </Navbar.Toggle>
-      <Navbar.Collapse>
-        <Nav>
-          <LinkContainer to={'/works'}><Nav.Link>Arbeiten</Nav.Link></LinkContainer>
-          <LinkContainer to={'/contact'}><Nav.Link>Kontakt</Nav.Link></LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="Navigation">
+      <Navbar expand='sm'>
+        <Navbar.Brand href={'/works'}>Michelle Akanji</Navbar.Brand>
+        <Navbar.Toggle className='navbar-toggle'>
+          <span className='icon-bar'/>
+          <span className='icon-bar'/>
+          <span className='icon-bar'/>
+        </Navbar.Toggle>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to={'/works'}><Nav.Link>Arbeiten</Nav.Link></LinkContainer>
+            <LinkContainer to={'/contact'}><Nav.Link>Kontakt</Nav.Link></LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 }
